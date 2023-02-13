ba38073cd4de7f269274370a941f4177d5eb0385
OleksandrKhlopenko <108742474+OleksandrKhlopenko@users.noreply.github.com>
2023-02-13 21:57:52 +0200

    react_card_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import "../src/App.css"
-import Card from './components/card/card'
+import "../src/App.css";
+import Card from "./components/card/Card";
 
 function App() {
-  
   return (
     <div className="App">
-       <Card >   
-      </Card>
+      <Card
+        title=" UX Design : How To Implement Usability Testing"
+        skillLevel="Beginner"
+        aughtor="Alfredo Rhiel Madsen"
+        students="500 Student"
+        modules="5"
+        duration="1.30"
+      />
     </div>
   );
 }
